be1d9ef80b0ff389e960359829371e8ebae878f9
Florian Klampfer <mail@qwtel.com>
2024-07-15 11:26:00 +0700

    Commit

--- src/comlink.ts
+++ src/comlink.ts
@@ -63,11 +63,11 @@ interface Proxy {
  *
  * This is the inverse of `Unpromisify<T>`.
  */
-type Promisify<T> = T extends PromiseLike<unknown> ? T : Promise<T>;
-
-type Asyncify<T> = T extends TupleOrRecordMarked
+type Promisify<T> = T extends TupleOrRecordMarked
   ? Promise<{ [P in keyof T as Exclude<P, TupleOrRecordMarker>]: T[P] }>
-  : Promisify<T>;
+  : T extends PromiseLike<unknown> 
+    ? T 
+    : Promise<T>;
 
 /**
  * Takes the raw type of a remote property and returns the type that is visible to the local thread on the proxy.
@@ -79,7 +79,7 @@ type RemoteProperty<T> =
   // If the value is a function, comlink will proxy it automatically.
   // Objects are only proxied if they are marked to be proxied.
   // Otherwise, the property is converted to a Promise that resolves the cloned value.
-  T extends Function | ProxyMarked ? Remote<T> : Asyncify<T>;
+  T extends Function | ProxyMarked ? Remote<T> : Promisify<T>;
 
 /**
  * Takes the raw type of a property as a remote thread would see it through a proxy (e.g. when passed in as a function
@@ -156,7 +156,7 @@ export type Remote<T> =
     (T extends (...args: infer TArguments) => infer TReturn
       ? (
           ...args: { [I in keyof TArguments]: UnproxyOrClone<TArguments[I]> }
-        ) => Asyncify<ProxyOrClone<Awaited<TReturn>>>
+        ) => Promisify<ProxyOrClone<Awaited<TReturn>>>
       : unknown) &
     // Handle construct signature (if present)
     // The return of construct signatures is always proxied (whether marked or not)
@@ -166,7 +166,7 @@ export type Remote<T> =
             ...args: {
               [I in keyof TArguments]: UnproxyOrClone<TArguments[I]>;
             }
-          ): Asyncify<Remote<TInstance>>;
+          ): Promisify<Remote<TInstance>>;
         }
       : unknown) &
     // Include additional special comlink methods available on the proxy.
